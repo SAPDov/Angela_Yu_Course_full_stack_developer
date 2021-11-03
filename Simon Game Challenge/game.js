5795c2c153463ecb350c662d64684b63c1ca0ac6
SAP <SD.NUTR@GMAIL.COM>
2021-11-03 18:37:25 +0200

    add functionality

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,55 +1,65 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-
-// chose a random color from the array
-var randomChosenColour = buttonColours[nextSequence()];
-
-// function generate a new random number between 0 and 3
-function nextSequence(){
-    var randonNumber = Math.floor(Math.random() * 3);
-    return randonNumber;
-};
-
-nextSequence();
-
-gamePattern.push(randomChosenColour);
-console.log(gamePattern);
+var userClickedPattern = [];
+var level = 0;
 
 
+$(document).keydown(function(event){
+    
+    $("#level-title").text("level " + level);
+    nextSequence();
+});
 
-
-
-$("#" + randomChosenColour).on("click", function(){
-    makeSound (randomChosenColour);
-    $(this).fadeOut(100).fadeIn(100);
+$(".btn").click(function (){
+    var userChosenColour = $(this).attr("id");
+    console.log(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+ //    $("#" + userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    makeSound(userChosenColour);
+    animatePress(userChosenColour);
+    console.log(userClickedPattern);
+    checkAnswer(userClickedPattern.length-1);
 });
 
 
-function handler (){
-    $(".btn")
-};
+function checkAnswer(currentLevel){
 
-// .makeSound(randomChosenColour);
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        setTimeout(function [])
+            nextSequence();
+    }
+}
+
+checkAnswer();
+
+// function generate a new random number between 0 and 3
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+// chose a random color from the array
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    // console.log(gamePattern); 
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    makeSound(randomChosenColour);
+    level ++;
+}
+
 
 function makeSound (key) {
-    switch(key){
-        case "red": 
-            var red = new Audio ("sounds/red.mp3");
-            red.play();
-            break; 
-        case "blue": 
-            var blue = new Audio ("sounds/blue.mp3");
-            blue.play();
-            break;
-        case "green": 
-            var green = new Audio ("sounds/green.mp3");
-            green.play();
-            break;
-        case "yellow": 
-            var yellow = new Audio ("sounds/yellow.mp3");
-            yellow.play();
-          break;
-    };
-};
+    var audio = new Audio ("sounds/" + key + ".mp3");
+    // audio.muted = true;
+    audio.play();
+}
+
+
+
+function animatePress(currentColour){
+        $("#" + currentColour).addClass("pressed");
+        setTimeout(function () {
+            $("#" + currentColour).removeClass("pressed");
+          }, 100);        
+
+}
+
 
