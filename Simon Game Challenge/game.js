4866399991992c5dc27c7fbe21d2242de00f28e3
SAP <SD.NUTR@GMAIL.COM>
2021-11-09 17:48:15 +0200

    fixed nextSequence function

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -8,48 +8,54 @@ var level = 0;
 
 $(document).keydown(function(event){
     if(!started) {
-        $("#level-title").text("level " + level);
-        nextSequence();
+        $("#level-title").text("Level " + level);
+        console.log("level is "+ level);
+        nextSequence();   
         started = true;
-    }
-   
+    } 
 });
 
 $(".btn").click(function (){
+
     var userChosenColour = $(this).attr("id");
-    console.log(userChosenColour);
+    // console.log(userChosenColour);
     userClickedPattern.push(userChosenColour);
- //    $("#" + userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     makeSound(userChosenColour);
     animatePress(userChosenColour);
+
     console.log(userClickedPattern);
     checkAnswer(userClickedPattern.length-1);
 });
 
 function checkAnswer(currentLevel){
-    console.log(userClickedPattern.length);
-    console.log(gamePattern.length);
+
+    // console.log(userClickedPattern.length);
+    // console.log(gamePattern.length);
 
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
         if (userClickedPattern.length === gamePattern.length){
+            console.log("success");
+
             setTimeout(function() {
                 nextSequence();
-             }, 500);
+             }, 1000);
            }
-        }
-    else {  
+        } else {  
         setTimeout(function () {
            $("#level-title").text("You are wrong");
-          }, 100);
-        }        
-        $("#level-title").text("Press A key to Start"); 
-        started = false;
-    
+          }, 100); 
+            console.log("wrong");
+            started = false;        
+            $("#level-title").text("Press A key to Start"); 
+        }          
     }
 
-
 // function generate a new random number between 0 and 3
-function nextSequence(){
+function nextSequence(){    
+    userClickedPattern = [];
+    level ++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
 // chose a random color from the array
     var randomChosenColour = buttonColours[randomNumber];
@@ -57,7 +63,6 @@ function nextSequence(){
     // console.log(gamePattern); 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     makeSound(randomChosenColour);
-    level ++;
 }
 
 function makeSound (key) {
