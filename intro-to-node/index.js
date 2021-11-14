ad807d160f61ad84de73070c43638c97c63e1127
SAP <SD.NUTR@GMAIL.COM>
2021-11-14 16:14:38 +0200

    node and packages

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+const superillains = require('supervillains');
+var mySuperheroName = superheroes.random();
+var mySuperVillain = superillains.random();
+
+console.log(mySuperheroName);
+console.log(mySuperVillain);
