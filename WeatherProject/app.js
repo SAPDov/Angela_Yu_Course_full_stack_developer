0ee6b40724a729b0632425be7b3a6903ed6f9648
SAP <SD.NUTR@GMAIL.COM>
2021-11-23 16:19:51 +0200

    final weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -13,31 +13,30 @@ app.get("/", function(req, res){
 })
 
 app.post("/", function(req, res){
-    console.log(req.body.cityName);
-})
 
-const query = "Israel";
-const apiKey = "";
-const units = "metric";
+    const query = req.body.cityName;
+    const apiKey = "";
+    const units = "metric";
 
-const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" + units;
-https.get(url, function(response){
-    console.log(response.statusCode);
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" + units;
+    https.get(url, function(response){
+        console.log(response.statusCode);
 
-    response.on("data", function(data){
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const weatherDescription = weatherData.weather[0].description;
-        const imageURL = weatherData.weather[0].icon;
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const imageURL = weatherData.weather[0].icon;
 
 
-        res.write("<p> The weather is currently "  + weatherDescription + "</p>");
-        res.write("<h1> The temperture in Israel is " + temp +  " degrees Celicius </h1>");
-        res.write("<img src = http://openweathermap.org/img/wn/" + imageURL + "@2x.png>");
-        res.send();
+            res.write("<p> The weather is currently "  + weatherDescription + "</p>");
+            res.write("<h1> The temperture in " + query + " is " + temp +  " degrees Celicius </h1>");
+            res.write("<img src = http://openweathermap.org/img/wn/" + imageURL + "@2x.png>");
+            res.send();
 
+        })
     })
-})
+ })
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
