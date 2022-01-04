518b2259c8bc0ad30b9659ed097de4b01e79e7b7
SAP <SD.NUTR@GMAIL.COM>
2022-01-04 15:52:38 +0200

    express routh params

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -8,7 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
+let posts = [];
 
 const app = express();
 
@@ -24,8 +24,11 @@ app.get("/", function(req,res){
     homeContent:homeStartingContent,
      posts:posts
   });
+  
   // console.log(posts);
 });
+
+
 
 app.get("/about", function(req,res){
   res.render("about",{aboutContent: aboutContent});
@@ -50,7 +53,19 @@ app.post("/compose", function (req, res){
     
 });
 
+app.get("/posts/:topic", function(req, res){
+  const requestedTitle = req.params.topic;
 
+  posts.forEach(function (post){
+    const storedTitle = post.title;
+    
+    if(storedTitle === requestedTitle){
+      console.log("Match Found");
+    } else {
+      console.log("No Match Found");
+    }
+  }); 
+});
 
 
 
