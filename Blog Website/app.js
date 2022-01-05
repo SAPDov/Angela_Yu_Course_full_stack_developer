6f3626c90f61523ae633b56b9f35e889b98b3a97
SAP <SD.NUTR@GMAIL.COM>
2022-01-05 15:51:09 +0200

    lodash

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,6 +12,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 let posts = [];
 
 const app = express();
+app.locals._ = _
 
 app.set('view engine', 'ejs');
 
@@ -20,14 +22,14 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req,res){
-  res.render("home", {
+
+    res.render("home", {
     homeContent:homeStartingContent,
-     posts:posts
+    posts:posts
+    
   });
   
-  // console.log(posts);
 });
-
 
 
 app.get("/about", function(req,res){
@@ -49,21 +51,22 @@ app.post("/compose", function (req, res){
     body:req.body.newPost,
   };
     posts.push(post);
-    res.redirect("/");
-    
+    res.redirect("/");   
 });
 
 app.get("/posts/:topic", function(req, res){
-  const requestedTitle = req.params.topic;
+  const requestedTitle = _.lowerCase(req.params.topic);
 
   posts.forEach(function (post){
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
     
     if(storedTitle === requestedTitle){
-      console.log("Match Found");
-    } else {
-      console.log("No Match Found");
-    }
+    
+      res.render("post", {
+        title:post.title,
+         body:post.body
+    });
+    } 
   }); 
 });
 
